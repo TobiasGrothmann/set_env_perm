ca831c3189cc3c216bc880eaed7b2da98f135fc7
Tobias Grothmann <tobias.grothmann@googlemail.com>
2026-01-18 17:12:40 +0100

    fix name

--- examples/test.rs
+++ examples/test.rs
@@ -1,9 +1,9 @@
-use set_env;
+use set_env_perm;
 
 fn main() {
-    // set_env::check_or_set("d", "world6777").expect("not working");
-    set_env::set("XXX", "man").unwrap();
-    set_env::set("XXXd", "man").unwrap();
-    set_env::append("PATH", "hello").unwrap();
-    set_env::prepend("PATH", "okkkk").unwrap();
+    // set_env_perm::check_or_set("d", "world6777").expect("not working");
+    set_env_perm::set("XXX", "man").unwrap();
+    set_env_perm::set("XXXd", "man").unwrap();
+    set_env_perm::append("PATH", "hello").unwrap();
+    set_env_perm::prepend("PATH", "okkkk").unwrap();
 }
